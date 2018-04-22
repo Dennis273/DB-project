27dfb847642a9d7e364e26ab124ec252c01dd2c1
Dennis273 <dennisc695@gmail.com>
2018-04-22 17:39:04 +0800

    Add Swagger as APIDoc into project

--- MyVideoManager/Startup.cs
+++ MyVideoManager/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using MyVideoManager.Models;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace MyVideoManager
 {
@@ -18,12 +19,24 @@ namespace MyVideoManager
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<WorkContext>(opt => opt.UseInMemoryDatabase("MyVideo"));
+            // Register the Swagger generator, defining one or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "MVM API", Version = "v0.1" });
+            });
             services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
         {
+            app.UseSwagger();
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+
             app.UseMvc();
         }
     }
